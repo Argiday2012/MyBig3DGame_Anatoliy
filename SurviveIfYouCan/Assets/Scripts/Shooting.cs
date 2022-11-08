7a2aebeee3b923e1cc921d4b6f426c551412359f
Argiday2012 <anatoli.sidorenkov0@gmail.com>
2022-11-08 21:12:26 +0200

    RealShootWithoutEffects

--- SurviveIfYouCan/Assets/Scripts/Shooting.cs
+++ SurviveIfYouCan/Assets/Scripts/Shooting.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     [SerializeField] private float _damage = 10f;
-    [SerializeField] private float _fireRate = 1f;
+    [SerializeField] private float _fireRate = 3f;
     [SerializeField] private float _range = 10f;
+    [SerializeField] private float _force = 125f;
 
     public Camera Cam;
+    private float _nextFire = 0f;
 
+
+    private void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time > _nextFire)
+        {
+            _nextFire = Time.time + 1f / _fireRate;
+            Shoot();
+        }
+    }
 
     void Shoot()
     {
@@ -18,6 +29,11 @@ public class Shooting : MonoBehaviour
         if (Physics.Raycast(Cam.transform.position, Cam.transform.forward, out hit, _range))
         {
             Debug.Log("Hit");
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * _force);
+            }
         }
     }
 
